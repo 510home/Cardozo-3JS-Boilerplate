59da817966f6b9a131f387802e37716a2de1ab05
510home <daniel@danielcardozo.com>
2026-04-01 10:56:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ const gltfLoader = new GLTFLoader();
 
 //------ Sphere animated texture  ---------
 */
-    const ball = new THREE.SphereGeometry( 3, 12, 8);
+    const ball = new THREE.SphereGeometry( 2, 8, 6);
     const barmaterial = new THREE.MeshStandardMaterial({ color: "#ce15f6" });
 
     const sphere = new THREE.Mesh(ball, barmaterial);
