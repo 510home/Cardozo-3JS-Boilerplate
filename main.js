94a0a4308d3385981815a190072e95a440c743b4
510home <daniel@danielcardozo.com>
2026-04-01 11:21:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ function applyWASD() {
 // ─── Loaders ─────────────────────────────────────────────────────────────────
 const textureLoader = new THREE.TextureLoader();
 const gltfLoader = new GLTFLoader();
-/*const alphaMap01 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/510home/Cardozo-3JS-Boilerplate/edit/texture-test/textures/bars.jpg');
+const alphaMap01 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/510home/Cardozo-3JS-Boilerplate/edit/texture-test/textures/bars.jpg');
     barmaterial.alphaMap01 = alphaMap01;
     barmaterial.alphaMap01.magFilter = THREE.NearestFilter;
     barmaterial.alphaMap01.wrapT = THREE.RepeatWrapping;
